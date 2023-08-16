1e7805b7335a39fc8d5d3880d5ee60f01e4c9cd8
yuriyskrypnyk <skrypnyk81@gmail.com>
2023-08-16 10:52:30 +0200

    Connect images to wheel for admin. Change language to it in settings.py

--- order/admin.py
+++ order/admin.py
@@ -2,5 +2,14 @@ from django.contrib import admin
 from .models import *
 
 
-admin.site.register(Wheel)
+class ImagesInLine(admin.TabularInline):  # or use admin.StackedInline for a different layout
+    model = WheelsImage
+    extra = 1
+
+
+class CustomerAdmin(admin.ModelAdmin):
+    inlines = [ImagesInLine]
+
+
+admin.site.register(Wheel, CustomerAdmin)
 admin.site.register(Order)
