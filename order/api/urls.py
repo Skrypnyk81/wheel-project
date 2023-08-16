16f3127b54cb16fbfeccf45989c6992a536bf841
yuriyskrypnyk <skrypnyk81@gmail.com>
2023-08-16 16:56:53 +0200

    Update detail-wheel path. Add list-wheel and filter-list-wheel path.

--- order/api/urls.py
+++ order/api/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from order.api.views import WheelCreateView, WheelAPIView
+from order.api.views import WheelCreateView, WheelAPIView, WheelsListAPIView, FilterListAPIView
 
 
 urlpatterns = [
     path("create-wheel/", WheelCreateView.as_view(), name="create_wheel"),
-    path("retrieve-wheel/<int:pk>/", WheelAPIView.as_view(), name="retrieve_wheel")
+    path("detail-wheel/<int:pk>/", WheelAPIView.as_view(), name="detail_wheel"),
+    path("list-wheel/", WheelsListAPIView.as_view(), name="retrieve_wheel"),
+    path("filter-list-wheel/", FilterListAPIView.as_view(), name="retrieve_wheel"),
 ]
