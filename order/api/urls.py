7153b4f02c3141a3d12f1b1fd347392b193dd9e4
yuriyskrypnyk <skrypnyk81@gmail.com>
2023-08-17 12:46:01 +0200

    Create url for order

--- order/api/urls.py
+++ order/api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from order.api.views import WheelCreateView, WheelAPIView, WheelsListAPIView, FilterListAPIView
+from order.api.views import WheelCreateView, WheelAPIView, WheelsListAPIView, FilterListAPIView, OrderCreateAPIView
 
 
 urlpatterns = [
@@ -7,4 +7,5 @@ urlpatterns = [
     path("detail-wheel/<int:pk>/", WheelAPIView.as_view(), name="detail_wheel"),
     path("list-wheel/", WheelsListAPIView.as_view(), name="retrieve_wheel"),
     path("filter-list-wheel/", FilterListAPIView.as_view(), name="retrieve_wheel"),
+    path("create-order/", OrderCreateAPIView.as_view(), name="create-order"),
 ]
