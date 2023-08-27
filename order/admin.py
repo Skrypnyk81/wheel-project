aabf6435a4fd0ff7d11d2f4872b85cf26b88212d
yuriyskrypnyk <skrypnyk81@gmail.com>
2023-08-27 10:13:32 +0200

    Add order to wheel model for admin site

--- order/admin.py
+++ order/admin.py
@@ -2,13 +2,22 @@ from django.contrib import admin
 from .models import *
 
 
-class ImagesInLine(admin.TabularInline):  # or use admin.StackedInline for a different layout
+class ImagesInLine(admin.TabularInline):
     model = WheelsImage
     extra = 1
 
 
+class OrderInline(admin.StackedInline):
+    model = Order
+    extra = 0  # set to 0 if you don't want empty forms
+    readonly_fields = [
+        'first_name', 'second_name', 'phone', 'street',
+        'city', 'state', 'postal_code', 'country', 'created_at'
+    ]
+
+
 class CustomerAdmin(admin.ModelAdmin):
-    inlines = [ImagesInLine]
+    inlines = [ImagesInLine, OrderInline]
 
 
 admin.site.register(Wheel, CustomerAdmin)
