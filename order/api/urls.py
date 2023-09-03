ac91a41278dd5c8628aee13d087702d7c65d9933
yuriyskrypnyk <skrypnyk81@gmail.com>
2023-09-03 21:15:22 +0200

    Pep8 update for urls.py

--- order/api/urls.py
+++ order/api/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
-from order.api.views import WheelCreateView, WheelAPIView, WheelsListAPIView, FilterListAPIView, OrderCreateAPIView
+from order.api.views import (WheelCreateView, WheelAPIView, WheelsListAPIView,
+                             FilterListAPIView, OrderCreateAPIView)
 
 
 urlpatterns = [
     path("create-wheel/", WheelCreateView.as_view(), name="create_wheel"),
     path("detail-wheel/<int:pk>/", WheelAPIView.as_view(), name="detail_wheel"),
     path("list-wheel/", WheelsListAPIView.as_view(), name="retrieve_wheel"),
-    path("filter-list-wheel/", FilterListAPIView.as_view(), name="retrieve_wheel"),
+    path("filter-list-wheel/", FilterListAPIView.as_view(),
+         name="retrieve_wheel"),
     path("create-order/", OrderCreateAPIView.as_view(), name="create-order"),
 ]
