948b6f41864ca07c585d218f249b54de90603674
yuriyskrypnyk <skrypnyk81@gmail.com>
2023-09-03 21:17:32 +0200

    Clean default files of vite-vue, add file endpoints with url to django endpoints

---
+++ vite-frontend/src/common/endpoints.js
@@ -0,0 +1,11 @@
+const baseEndpoint="/api/v1/wheels/"
+
+const enpoints = {
+    createWheel: `${baseEndpoint}create-wheel/`,
+    detailWheel: `${baseEndpoint}detail-wheel/`,
+    listWheel: `${baseEndpoint}list-wheel/`,
+    filterWheel: `${baseEndpoint}filter-list-wheel/`,
+    createOrder: `${baseEndpoint}create-order/`,
+};
+
+export{ enpoints }
