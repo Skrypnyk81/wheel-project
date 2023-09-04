a85d8313b3228514af8c563d290db80c7870a3a5
yuriyskrypnyk <skrypnyk81@gmail.com>
2023-09-04 08:38:57 +0200

    Create Navbar

---
+++ vite-frontend/src/components/NavbarComponent.vue
@@ -0,0 +1,30 @@
+<template>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container">
+    <a class="navbar-brand" href="#">Gomme usate</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item">
+            <button type="button" class="btn btn-info">Contatti</button>
+        </li>
+       
+      </ul>
+    </div>
+  </div>
+</nav>
+</template>
+
+<script>
+export default {
+    name: "NavbarComponent"
+}
+</script>
+
+<style>
+.my-class{
+    color: red;
+}
+</style>
