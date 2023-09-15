dd5f5e733d712639844bf523df00fb388dc499fe
yuriyskrypnyk <skrypnyk81@gmail.com>
2023-09-15 18:40:49 +0200

    Add page if a wheel doesn't exist

--- vite-frontend/src/views/WheelDetail.vue
+++ vite-frontend/src/views/WheelDetail.vue
@@ -1,20 +1,25 @@
 <template>
     <div class="container">
-        <div id="carouselExample" class="carousel slide">
-        <div class="carousel-inner">
-            <div v-for="(image, index) in wheel.images" :key="index" class="carousel-item" :class="{ 'active': index === 0 }">
-                <img :src="image.image" class="d-block w-100" :alt="image.id">
+        <div v-if="wheel">
+            <div id="carouselExample" class="carousel slide">
+                <div class="carousel-inner">
+                    <div v-for="(image, index) in wheel.images" :key="index" class="carousel-item" :class="{ 'active': index === 0 }">
+                        <img :src="image.image" class="d-block w-100" :alt="index">
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
             </div>
         </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Next</span>
-        </button>
-    </div>
+        <div v-else>
+            <h1 class="text-danger text-center">Le ruote non ci sono!</h1>
+        </div>
     </div>
 </template>
 
@@ -26,7 +31,7 @@ import { endpoints } from '../common/endpoints';
         name: "WheelDetail",
         props: {
             id: {
-                type: Number,
+                type: String,
                 required: true
             }
         },
@@ -41,15 +46,14 @@ import { endpoints } from '../common/endpoints';
             try {
                 const response = await axios.get(endpoint);
                 this.wheel = response.data;
-            } catch {
+            } catch (error) {
                 console.log(error);
-                alert(error.response.statusText);
+                this.wheel = null;
             }
         }
     },
     created() {
         this.getWheelDetail()
-    }
-    }
-    
+        }
+    }  
 </script>
