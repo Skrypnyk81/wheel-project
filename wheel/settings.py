9f45b3438edf71205a28be19a2a906ef8c09846c
yuriyskrypnyk <skrypnyk81@gmail.com>
2023-10-07 20:36:05 +0200

    remove sentry dns

--- wheel/settings.py
+++ wheel/settings.py
@@ -160,7 +160,7 @@ EMAIL_HOST_PASSWORD = os.environ.get('PASSWORD_SEND_EMAIL')
 
 # sentry
 sentry_sdk.init(
-    dsn="https://0f39c498caa85ccde3f244a5bfcc2210@o4506010011828224.ingest.sentry.io/4506010013335552",
+    dsn= os.environ.get('SENTRY_DSN'),
     # Set traces_sample_rate to 1.0 to capture 100%
     # of transactions for performance monitoring.
     traces_sample_rate=1.0,
