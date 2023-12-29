27f4c2f82d85bea3645f66f000a9d8e717c10fa3
yuriyskrypnyk <skrypnyk81@gmail.com>
2023-12-29 17:20:31 +0100

    Create ContactsView.vue

---
+++ vite-frontend/src/views/ContactsView.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="container">
+        <div align-items-center>
+            <p>
+                Benvenuti da Nora Pneumatici, la destinazione premium per chi cerca la perfetta combinazione tra sicurezza stradale e risparmio. Siamo un'azienda specializzata nella vendita di pneumatici usati di altissima qualità, dedicati a coloro che desiderano percorrere la strada con fiducia senza dover rinunciare al portafoglio.
+            </p>
+            <p>
+                Da Nora Pneumatici, crediamo che la sicurezza sia fondamentale e che la qualità non debba mai essere compromessa. I nostri esperti selezionano con cura ogni pneumatico usato, garantendo prestazioni elevate e durata affidabile. Ogni gomma che lascia il nostro magazzino è sottoposta a rigorosi controlli per assicurare che soddisfi i nostri standard elevati e le aspettative dei nostri clienti.
+            </p>
+            <p>
+                Ci impegniamo a offrire un'ampia gamma di marche e modelli per adattarsi alle esigenze di ogni veicolo. Con Nora Pneumatici, non solo risparmierai sul costo, ma avrai anche la tranquillità di guidare su pneumatici che rispondono agli standard più elevati di sicurezza e affidabilità.
+            </p>
+            <p>
+                Scegli Nora Pneumatici per un viaggio senza preoccupazioni, dove la qualità e il risparmio si incontrano sulla strada. Affidati a noi per un'esperienza di guida sicura, conveniente e di alta qualità.
+            </p>
+            <br>
+            <p>
+                <i class="bi bi-envelope-at"></i>
+                <a href="mailto:info@norapneumatici.it"> info@norapneumatici.it</a>
+            </p>
+            <p>
+                <i class="bi bi-phone-vibrate"></i>
+                <a href="tel:+393272660876"> +39 327 266 08 76</a>
+            </p>
+            <p>
+                <i class="bi bi-house-door"> via Enrico Fermi 4, Treviglio(BG) 24047</i>
+            </p>
+            
+        </div>
+    </div>
+
+</template>
+<script>
+export default {
+    created() {
+        document.title = "Contatti"
+    }
+}
+</script>
